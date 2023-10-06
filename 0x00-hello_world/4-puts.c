9b382c939db7025cf00efbb72f49f861dd15ab38
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-06 07:29:43 -0700

    edit 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /*
- * main - this is the main put function
+ * main - this is the entry point
+ *
+ * Description: this is the main function which serves as the entry point.
+ * it prints a message to the standard output
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
