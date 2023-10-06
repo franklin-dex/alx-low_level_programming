4a46038d8963726f8e27ed72db88eff4d247324a
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-06 07:18:35 -0700

    edit 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
-
-/**
- * main- this is the main put function
- * Return; 0
+#include <stdio.io>
+/*
+ * main - this is the main put function
+ * Return: 0
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
