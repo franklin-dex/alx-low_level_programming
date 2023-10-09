a1a19c566e0dcd3ea065396a9cfc77b124877b4b
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-09 03:50:29 -0700

    add 4-put file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
-/*
+/**
  * main - Entry point
  *
- * Description:This is the main function that serves as the entry point
+ * Description: This is the main function that serves as the entry point of the program.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-       	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
