819a7b1617e061c6b4d6ede34b3d2a5fe8397168
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-09 21:02:09 -0700

    ediited the 0positive file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("The number is: %d\n", n);
+	printf("The number is:%d\n", n);
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n "");
