1cf13c4f97ebf1f63b3365b22de39eda3c23cf92
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-11 15:31:50 -0700

    edit 9 times file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,21 +16,23 @@ void times_table(void)
 			if (j > 0)
 			{
 				_putchar(',');
-				_putchar(' ');
+				_putchar('	');
+				if (product < 10)
+				{
+					_putchar('	');
+				}
 			}
 
 			if (product < 10)
 			{
-				_putchar('\t');
-				_putchar(product + '0');
+				_putchar('0' + product);
 			}
 			else
 			{
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				_putchar('0' + product / 10);
+				_putchar('0' + product % 10);
 			}
 		}
 		_putchar('\n');
 	}
 }
-
