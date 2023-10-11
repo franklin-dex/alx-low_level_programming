2f445a33c79eecfd677e43837ea65a3e9ae403b5
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-11 15:46:54 -0700

    add 100 timestable file

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The times table to print.
+ *
+ * Return: void.
+ */
+void print_times_table(int n)
+{
+    if (n > 15 || n < 0)
+        return;
+
+    int i, j, product;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            product = i * j;
+
+            if (j == 0)
+            {
+                printf("%d", product);
+            }
+            else
+            {
+                printf("\t%d", product);
+            }
+        }
+        printf("\n");
+    }
+}
