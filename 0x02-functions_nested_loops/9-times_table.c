5f07c6311203b60b2050eb5069ce799ec7b4b589
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-11 15:16:41 -0700

    add 9 times file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,31 +5,35 @@
  */
 void times_table(void)
 {
-	int i, j, product;
+    int i, j, product;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			product = i * j;
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            product = i * j;
 
-			if (j > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+            if (j > 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (product < 10)
+                {
+                    _putchar(' ');
+                }
+            }
 
-			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + product);
-			}
-			else
-			{
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
-			}
-		}
-		_putchar('\n');
-	}
+            if (product < 10)
+            {
+                _putchar('0' + product);
+            }
+            else
+            {
+                _putchar('0' + product / 10);
+                _putchar('0' + product % 10);
+            }
+        }
+        _putchar('\n');
+    }
 }
+
