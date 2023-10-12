c3b68104d7a0421924ca4f4106eda9119b971264
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-12 13:52:30 -0700

    add 5-more_num file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints ten times of a number
+ *
+ * Return: 0 always
+ */
+void more_numbers(void)
+{
+	char j, c;
+
+	for (c >= 0; c <= 9; c++)
+	{
+		for (j >= 0; j <= 14; j++)
+		{
+			if (c >= 10)
+			{
+				_putchar('c');
+				_putchar(j % 10 + '0');
+			}
+
+			else
+			{
+				_putchar(c + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
