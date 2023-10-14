6c6ff402465b523790112dabd9734f5147e8bcec
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-14 08:42:46 -0700

    add 102-print_comb5 file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar('\t');
+			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
