22ef43cd6ae8ec1ba3fe5d0e26a709a6ce08e7c6
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-14 07:13:28 -0700

    add 100print_comb3 file

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = a + 1; b < 10; b+++)
+		{
+			putchar((a % 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 8 && b == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return 0;
+}
+
+
