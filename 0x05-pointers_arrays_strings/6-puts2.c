9f73d6f3416d42ba35cdcb61f470e5f5f3abffa1
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-17 13:02:17 -0700

    edit 6 puts file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,13 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i == 0; str[i] != '\0'; i++)
 	{
-		putchar(str[i]);
-		i += 2;
-	}
+		while (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
 	putchar('\n');
 }
