e2f1fbfa8ce4606592bdfc4ef167c232ebd9ce44
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-16 21:43:47 -0700

    add 0 reset file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-void reset-to_98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
@@ -11,7 +11,7 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(cha *dest, char *src);
+Char *_strcpy(char *dest, const char *src);
 int _atoi(char *s);
 
 #endif
