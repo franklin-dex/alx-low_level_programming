61c391fc0b18c4089d7199a60d69115fed2bb140
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-16 22:47:58 -0700

    add 5 rev string file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,16 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
+	int length = 0;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    for (int i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
-
-    _putchar('\n');
+	for (int i = length - 1; i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
 }
