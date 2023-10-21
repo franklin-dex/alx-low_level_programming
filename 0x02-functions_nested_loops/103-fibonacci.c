6d2e281e4abd408df8c1596b91e5c156d6835f30
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-21 09:25:42 -0700

    added 103 fib file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,25 +5,31 @@
  *
  * Return: always 0
  */
-
 int main(void)
 {
-	long long int prev = 1, current = 2, next;
-	long long int sum = 0;
+	int prev = 1, current = 2, next;
+	int sum = 0;
+
+	printf("%d,%d,", prev, current);
 
 	while (current <= 4000000)
 	{
-		if (current % 2 == 0)
-		{
-			sum += current;
-		}
-
 		next = prev + current;
+		if (next % 2 == 0)
+		{
+			sum += next;
+		}
 		prev = current;
 		current = next;
+		if (current <= 4000000)
+		{
+			printf("%d", current);
+			if (current <= 3999998)
+				printf(",");
+		}
 	}
 
-	printf("%lld\n", sum);
+	printf("\nSum of even-valued terms:%d\n", sum);
 
 	return (0);
 }
