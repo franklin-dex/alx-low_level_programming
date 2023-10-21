12b2b57ae34d5c04fe174c6cdc8e9dc41f7039d3
Frankiln <odinakachukwufranklin0@gmail.com>
2023-10-21 08:42:16 -0700

    add

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,16 +18,17 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			
+
 			if (product < 10)
 			{
 				_putchar(' ');
-				_putchar('0' + product);
+				_putchar(' ');
+				_putchar(product + '0');
 			}
 			else
 			{
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
 			}
 		}
 		_putchar('\n');
