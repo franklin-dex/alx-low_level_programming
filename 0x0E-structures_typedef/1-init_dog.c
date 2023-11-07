adb51237275499ca155ecce5e7eb3c1f6b7966e9
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-07 02:12:50 -0800

    add init_dog file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include "main.h"
 
 /**
  * init_dog - Initializes a struct dog
@@ -10,7 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d != 0)
 	{
 		d->name = name;
 		d->age = age;
