20f2bb70e083e4d8f8008870a53e66d5f5dbb28e
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-08 20:59:34 -0800

    add

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(in - Entry point
+		 * @argc: The number of command-line arguments
+		 *  * @argv: An array of command-line argument strings
+		 *   *
+		 *    * Return: 0 on success, 1 for incorrect number of arguments, 2 for negative input
+		 *     */iiint argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num_bytes = atoi(argv[1]);
+
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+
+	char *main_address = (char *)main;
+
+	for (int i = 0; i < num_bytes; i++)
+	{
+		printf("%02hhx", main_address[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+
+	return (0);
+}
