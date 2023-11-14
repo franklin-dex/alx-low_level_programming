6e6070eb267522132921e2df7d227d742d554585
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-13 16:14:02 -0800

    add main

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int check_args(int argc);
+int parse_cents(char *arg);
+int calculate_coins(int cents);
+void print_minimum_coins(int min_coins);
+
+#endif
+
