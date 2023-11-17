f7b33d232d78a260987d660b633dc499acadc0ff
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-16 19:05:24 -0800

    add hello holberton file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,15 @@
-section .data
-    hello db 'Hello, Holberton', 0
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global _start
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-_start:
-    ; write Hello, Holberton to stdout
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, hello
-    mov edx, 16
-    int 0x80
-
-    ; exit
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+	mov eax, 0
+	ret
