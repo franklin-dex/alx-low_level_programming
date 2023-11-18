0739aef4916b8aa337cae6d91290a6f1dee7498b
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-18 06:34:52 -0800

    edit change file

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: 1 on success, -1 on error
+ */
+int _putchar(char c);
+
+/**
+ * check_args - Checks the number of arguments
+ * @argc: Argument count
+ * Return: 0 if argc is 2, else 1
+ */
+int check_args(int argc);
+
+/**
+ * parse_cents - Parses the cents argument
+ * @arg: Cents argument as a string
+ * Return: Parsed cents as an integer
+ */
+int parse_cents(char *arg);
+
+/**
+ * calculate_coins - Calculates the minimum number of coins
+ * @cents: Amount in cents
+ * Return: Minimum number of coins
+ */
+int calculate_coins(int cents);
+
+/**
+ * print_minimum_coins - Prints the minimum number of coins
+ * @min_coins: Minimum number of coins
+ */
+void print_minimum_coins(int min_coins);
 
 /**
  * main - Entry point
- * @argc: The number of command line arguments
- * @argv: An array containing the command line arguments
- *
- * Return: 0 if successful, 1 if there is an error
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+	int cents;
+	int min_coins;
 
-	int cents = atoi(argv[1]);
+	if (check_args(argc) != 0)
+		return (1);
+
+	int cents = parse_cents(argv[1]);
 
 	if (cents < 0)
 	{
@@ -25,38 +57,67 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	int coins = 0;
+	int min_coins = calculate_coins(cents);
 
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-		{
-			cents -= 5;
-			coins++;
-		}
-		else if (cents >= 2)
-		{
-			cents -= 2;
-			coins++;
-		}
-		else
-		{
-			cents -= 1;
-			coins++;
-		}
-	}
-
-	printf("%d\n", coins);
+	print_minimum_coins(min_coins);
 
 	return (0);
 }
+
+/**
+ * check_args - Checks the number of arguments
+ * @argc: Argument count
+ * Return: 0 if argc is 2, else 1
+ */
+int check_args(int argc)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * parse_cents - Parses the cents argument
+ * @arg: Cents argument as a string
+ * Return: Parsed cents as an integer
+ */
+int parse_cents(char *arg)
+{
+	return (atoi(arg));
+}
+
+/**
+ * calculate_coins - Calculates the minimum number of coins
+ * @cents: Amount in cents
+ * Return: Minimum number of coins
+ */
+int calculate_coins(int cents)
+{
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coins = sizeof(coins) / sizeof(coins[0]);
+	int min_coins = 0;
+
+	for (int i = 0; i < num_coins; i++)
+	{
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			min_coins++;
+		}
+	}
+
+	return (min_coins);
+}
+
+/**
+ * print_minimum_coins - Prints the minimum number of coins
+ * @min_coins: Minimum number of coins
+ */
+void print_minimum_coins(int min_coins)
+{
+	printf("%d\n", min_coins);
+}
+
