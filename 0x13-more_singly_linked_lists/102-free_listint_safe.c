595a03f0212728b85ace04fb209ade229037bcaa
Frankiln <odinakachukwufranklin0@gmail.com>
2023-11-21 13:34:42 -0800

    add free list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - Frees a listint_t linked list safely.
+ * @h: Pointer to a pointer to the head of the list.
+ *
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *current, *temp;
+	size_t count = 0;
+
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	current = *h;
+
+	while (current != NULL)
+	{
+		count++;
+
+		temp = current;
+		current = current->next;
+
+		free(temp);
+
+		if (temp <= current)
+		{
+			*h = NULL;
+			break;
+		}
+	}
+
+	*h = NULL;
+
+	return (count);
+}
