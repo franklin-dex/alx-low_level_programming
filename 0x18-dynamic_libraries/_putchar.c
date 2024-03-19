6c32509bfc9d1f3bf4fcb1e82d44bad97bec886e
Frankiln <odinakachukwufranklin0@gmail.com>
2024-03-18 22:27:56 -0700

    updated the dynamic library

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,4 +1,11 @@
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - prints a char to stdout
+ * @c - the character to be printed
+ * return: on success the num of char written
+ * on error -1 is returned and errno is set appropriately
+ */
 
 int _putchar(char c)
 {
